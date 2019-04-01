e42a69307482a4c5aa88af54960d77763ea38a87
LazyMechanic <asharnrus@mail.ru>
2019-04-01 14:11:50 +0300

    Add scripts

---
+++ Assets/Scripts/Spaceship/SpaceshipControl.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Assertions;
+
+public class SpaceshipControl : MonoBehaviour
+{
+    public Rigidbody rigidbody;
+
+    public float speed = 100.0f;
+    [SerializeField]
+    private bool canJump = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Assert.IsNotNull(rigidbody, "[SpaceshipControl]: Spaceship rigidbody is null");
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rigidbody.velocity = rigidbody.transform.forward * speed;
+    }
+}
